6c6f04930875c1e7e556b6bb67513763183f286d
nRamirezP <colachoramirez5@gmail.com>
2016-05-14 15:55:37 -0500

    Database Ok

--- app/src/main/java/com/fisiotips/unisabana/fisiotips/DBHandler.java
+++ app/src/main/java/com/fisiotips/unisabana/fisiotips/DBHandler.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by nRamirezP on 5/13/16.
@@ -32,14 +33,19 @@ public class DBHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db)
     {
         String CREATE_CONTACTS_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_EXERCISES + "("
-                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " VARCHAR,"
-                + KEY_DESCRP + " VARCHAR" + KEY_DESCRP+" VARCHAR"
-                +KEY_TIP+" VARCHAR"+ KEY_IMG+" VARCHAR"+")";
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_DESCRP + " TEXT,"+ KEY_TIP +" TEXT,"+ KEY_IMG+" TEXT"+")";
         db.execSQL(CREATE_CONTACTS_TABLE);
+        Log.i("llave",KEY_TIP);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+    public void onUpgrade(SQLiteDatabase db)
     {
         // Drop older table if existed
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_EXERCISES);
@@ -54,23 +60,28 @@ public class DBHandler extends SQLiteOpenHelper {
 
     public void addExcersice (Exercise ejercicio)
     {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(KEY_ID, ejercicio.getId());
-        values.put(KEY_NAME, ejercicio.getNombre());
-        values.put(KEY_DESCRP, ejercicio.getDes());
-        values.put(KEY_TIP, ejercicio.getTip());
-        values.put(KEY_IMG, ejercicio.getImg());
+        if(traeEjercicio(ejercicio.getId())==null)
+        {
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put(KEY_ID, ejercicio.getId());
+            values.put(KEY_NAME, ejercicio.getNombre());
+            values.put(KEY_DESCRP, ejercicio.getDes());
+            values.put(KEY_TIP, ejercicio.getTip());
+            values.put(KEY_IMG, ejercicio.getImg());
 
-        db.insert(TABLE_EXERCISES, null, values);
-        db.close();
+            db.insert(TABLE_EXERCISES, null, values);
+
+            db.close();
+        }
     }
 
     public Exercise traeEjercicio(int id)
     {
+        Exercise ejercicio = null;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query(TABLE_EXERCISES, new String[] { KEY_ID,
-                        KEY_NAME, KEY_DESCRP, KEY_DESCRP, KEY_TIP, KEY_IMG },
+                        KEY_NAME, KEY_DESCRP, KEY_TIP, KEY_IMG },
                         KEY_ID + "=?",
                         new String[]
                                 {
@@ -80,11 +91,12 @@ public class DBHandler extends SQLiteOpenHelper {
 
         if (cursor != null)
             cursor.moveToFirst();
-        Exercise ejercicio = new Exercise(Integer.parseInt(cursor.getString(0)),
+        ejercicio = new Exercise(Integer.parseInt(cursor.getString(0)),
                                             cursor.getString(1),
                                             cursor.getString(2),
                                             cursor.getString(3),
                                             cursor.getString(4));
         return ejercicio;
     }
+
 }
